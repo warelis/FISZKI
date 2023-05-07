29ccf1bd0fd8df1c1d5f3f7d54dff305a831c81a
warelis <122726481+warelis@users.noreply.github.com>
2023-05-07 17:25:27 +0200

    Update AloneWordRepository.java

--- src/main/java/pl/warelis/flashcards/repository/AloneWordRepository.java
+++ src/main/java/pl/warelis/flashcards/repository/AloneWordRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.warelis.flashcards.model.AloneWord;
 
 public interface AloneWordRepository extends JpaRepository<AloneWord, Long> {
-
+ 
 }
